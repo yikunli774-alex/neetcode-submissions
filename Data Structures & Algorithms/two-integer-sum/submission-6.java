98ee9b1e40dc766cc5778eb670934ea23ade56a9
Alex <yikunli774@gmail.com>
2026-04-21 23:16:08 -0500

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++ ) {
+            int current_num = nums[i];
+            int complement = target - current_num;
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            } else {
+                map.put(nums[i],i);
+            }
+        }
+
+        return new int[]{-1, -1};
+
+    }
+}
